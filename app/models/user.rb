ed2b5c16736e47131cbab7a8489de05b353d26b0
Ravi <i.rrpatel@gmail.com>
2017-08-04 07:55:49 -0500

    add users model, migration, controller, and view

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+
+  validates_presence_of :email, :hashed_password
+  validates_uniqueness_of :email
+  validate :password_length
+
+  def password
+    @password ||= Bcrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @plain_password = new_password
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def password_length
+    errors.add(:password, 'must be at least 7 characters long') unless @plain_password.length >= 6
+  end
+
+  def authenticate(plain_text_password)
+    self.password == plain_text_password
+  end
+
+end
