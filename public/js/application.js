8dccd10871a529610f01c937210b52c5fa47dc11
Ravi <i.rrpatel@gmail.com>
2017-08-24 21:48:45 -0500

    Ajaxification of button functional; filter logic bug present

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
   generateRestaurantListener();
 });
+
+function generateRestaurantListener() {
+  $("#generator-form").on("click", function(event) {
+    event.preventDefault();
+    $buttonForm = $(this);
+    buttonUrl = $buttonForm.attr("action");
+    buttonMethod = $buttonForm.attr("method");
+
+    promise = $.ajax({
+      url: buttonUrl,
+      method: buttonMethod
+    });
+
+    promise.done(function(response) {
+      $(".error_messages").remove()
+      $buttonForm.hide();
+      $("#main-header").find("small").append(response);
+    });
+
+  });
+};
