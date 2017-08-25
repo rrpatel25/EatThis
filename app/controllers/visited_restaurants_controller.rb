0c9990c7beb9a3b66928dcfa76a758acf5c42cff
Ravi <i.rrpatel@gmail.com>
2017-08-25 13:07:49 -0500

    Add visited restaurants index display and controller route

--- app/controllers/visited_restaurants_controller.rb
+++ app/controllers/visited_restaurants_controller.rb
@@ -1,6 +1,6 @@
 get '/visited_restaurants/index' do
+  authenticate!
   # gather all of the user's visited restaurants and display in list
-
-
+  @conquered_restaurants = current_user.visited_restaurants
   erb :'visited_restaurants/index'
 end
